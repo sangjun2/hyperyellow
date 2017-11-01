6a4b78ebe6352ee777cec09cab36d3a0283b2b4d
sangjun2 <sj27294626@gmail.com>
2017-11-01 21:48:44 +0900

    color modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.decorateConfig = (config) => {
   return Object.assign({}, config, {
-    borderColor: 'yellow',
-    cursorColor: 'yellow',
+    borderColor: 'blue',
+    cursorColor: 'blue',
     css: `
       ${config.css || ''}
       .tabs_nav .tabs_list .tab_text {
